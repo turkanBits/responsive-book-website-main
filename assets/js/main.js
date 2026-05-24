003e7aaa51a9089e06a61a2ca68b0906abd4eaeb
bturkan <bayramovat2005@gmail.com>
2026-05-24 15:39:52 +0400

    add smooth sliding showcase

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,25 @@ if(loginClose){
 
 
 /*=============== HOME SWIPER ===============*/
+ let swiperHome = new Swiper('.home__swiper', {
+  loop: true,
+  spaceBetween:-24,
+  grabCursor:true,
+  slidesPerView:'auto',
+  centeredSlides:'auto',
 
+  autoplay: {
+   delay:3000,
+   disableOnInteraction:false,
+  },
+
+  breakpoints: {
+   1220: {
+      spaceBetween:-32,
+   }
+  }
+})
+ 
 
 /*=============== FEATURED SWIPER ===============*/
 
